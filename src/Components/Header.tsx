de11ee50eace794bc67bface51885064fe2d78d5
chaechef <chaeefe@gmail.com>
2020-03-07 23:53:40 +0900

    style: menu toggle

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/images/todoapp.png'
 import menu from '../assets/icons/menu.png'
 import './header.scss'
@@ -6,11 +6,32 @@ import './header.scss'
 type HeaderProps = {}
 
 function Header({}: HeaderProps) {
+  const [sideToggle, setSideTogge] = useState(false)
+
   return (
     <div className="header">
-      <div className="menu">
+      <div
+        className="menu"
+        onClick={() => {
+          setSideTogge(!sideToggle)
+        }}
+      >
         <img src={menu} alt="menu" />
       </div>
+      {sideToggle ? (
+        <div className="side-menu">
+          <button
+            onClick={() => {
+              setSideTogge(!sideToggle)
+            }}
+          >
+            x
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+
       <div className="logo">
         <a href="/">
           <img src={logo} alt="logo"></img>
